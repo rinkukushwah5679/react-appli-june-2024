f9048426076be1ea1fb0ef940cf31801ca0cd922
Arvind Kushwah <arvindkushwah9@gmail.com>
2024-10-07 10:46:06 +0530

    Fixed error after login time run application Readme.rb

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,22 @@
-import React from 'react'
-
+import React, {useEffect, useState} from 'react'
+import axios from 'axios';
 function About() {
+  const [users, setUsers] = useState([]);
+// import { BASE_URL } from '../../config';
+
+  const getUsers = () => async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/users");
+      debugger
+
+    } catch (error) {
+
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [])
   return (
     <div>
       <h1>About page</h1>
